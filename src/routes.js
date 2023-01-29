f575a6293686525c765e45ca07336053ed44eeeb
Bruno Goniadis <brunogoniadislima@gmail.com>
2023-01-29 02:35:22 -0300

    Feat: PriorityController

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ const routes = express.Router();
 
 
 const AnnotationControler = require('./controllers/AnnotationController');
+const ContentController = require('./controllers/ContentController');
+const PriorityController = require('./controllers/PriorityController');
 
 routes.post('/annotations', AnnotationControler.create);
 routes.get('/annotations', AnnotationControler.read);
 routes.delete('/annotations/:id', AnnotationControler.delete);
 
+routes.get('/priorities', PriorityController.read);
+routes.post('/priorities/:id', PriorityController.update);
+
+routes.post('/contents/:id', ContentController.update);
 module.exports = routes;
